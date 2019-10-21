b07d6027fc8e83b142f9cc98621653c17ae7ae21
mesielepush <48403308+mesielepush@users.noreply.github.com>
2019-10-21 12:05:00 -0500

    eliminate repeated code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -21,28 +21,6 @@ def bubble_sort(list)
     return list
 end
 
-def bubble_sort(list)
-    iter = list.size - 2
-    max_iter = list.size - 2
-    while max_iter >= 2
-        swap = false
-        0.upto(iter) do |i|
-            if list[i] > list[i+1]
-                list[i], list[i+1] = list[i+1], list[i]
-                swap = true
-            end
-        end
-
-        max_iter -= 1
-        puts 'max_iter: ', max_iter
-        if swap == false
-            break
-        end
-    end
-    return list
-end
-
-
 def bubble_sort_by(list)
   loop do
       swap = false
