f99c20d7b73bcdc3abe6dbadb29ef45224e3845b
jdmartinez1062 <jd.martinez1062@gmail.com>
2019-10-21 11:50:39 -0500

    defined bubble_sort_by function and added  some code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,36 @@
 list = [9,8,7,4,5,6,1,2,3]
+
 def bubble_sort(list)
+    iter = list.size - 2
+    max_iter = list.size - 2
+    while max_iter >= 2
+        swap = false
+        0.upto(iter) do |i|
+            if list[i] > list[i+1]
+                list[i], list[i+1] = list[i+1], list[i]
+                swap = true
+            end
+        end
+
+        max_iter -= 1
+        puts 'max_iter: ', max_iter
+        if swap == false
+            break
+        end
+    end
+    return list
+end
+
+def bubble_sort_by(list)
+    
+    lengths = Array.new
+    j=0
+
+    for words in list 
+        lengths[j]=words.length
+        j += 1
+    end
+
     iter = list.size - 2
     max_iter = list.size - 2
     while max_iter >= 2
